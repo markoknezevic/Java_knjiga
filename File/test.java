d40541f2dbd5143de2445fa698ab8b5dd3131f3b
markoknezevic <markoknezevic75@gmail.com>
2018-07-14 16:36:09 +0200

    Zavrsen fajl

---
+++ File/test.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+class test{
+    public static void main(String []args)throws IOException
+    {
+        try{
+FileInputStream in=new FileInputStream("tek.txt");
+
+        }
+        finally{
+
+        }
+    }
+}
