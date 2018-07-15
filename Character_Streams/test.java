334d340d09736f91c071ea8f7641f93987cea577
markoknezevic <markoknezevic75@gmail.com>
2018-07-15 23:02:39 +0200

    Novo

--- Character_Streams/test.java
+++ Character_Streams/test.java
@@ -1,3 +1,21 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 class test{
-    public static void main(String []args)
+    public static void main(String []args)throws IOException
+    {
+    
+    try{
+        FileReader in =new FileReader("tek.txt");
+   FileWriter out =new FileWriter("out.txt");
+        int c;
+        while((c=in.read())!=-1)
+        out.write(c);
+    }
+    finally{
+        
+
+    }
+    }
 }
