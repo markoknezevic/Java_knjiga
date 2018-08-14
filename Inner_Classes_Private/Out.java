838545c6becee68292ab904a324283e0cc97f0da
markoknezevic <markoknezevic75@gmail.com>
2018-08-14 23:24:12 +0200

    Novo

---
+++ Inner_Classes_Private/Out.java
@@ -0,0 +1,11 @@
+class Out{
+    private int a;
+    public Out(int v){
+        a=v;
+    }
+    public class Iner{
+        public void Isp(){
+            System.out.println(a);
+        }
+    }
+}
---
+++ Inner_Classes_Private/test.java
@@ -0,0 +1,7 @@
+class test{
+    public static void main(String[]args){
+        Out c=new Out(2);
+        Out.Iner b=c.new Iner();
+        b.Isp();
+    }
+}
